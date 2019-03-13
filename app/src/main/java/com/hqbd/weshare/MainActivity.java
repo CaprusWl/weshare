4574dc89c08f6c4743b9818d1327164b84743616
capruswl <capruswl@gmail.com>
2019-03-13 19:30:37 +0800

    first commit

---
+++ app/src/main/java/com/hqbd/weshare/MainActivity.java
@@ -0,0 +1,44 @@
+package com.hqbd.weshare;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
+import cn.smssdk.gui.RegisterPage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendCode(this);
+    }
+
+    public void sendCode(final Context context) {
+        RegisterPage page = new RegisterPage();
+        //如果使用我们的ui，没有申请模板编号的情况下需传null
+        page.setTempCode(null);
+        page.setRegisterCallback(new EventHandler() {
+            public void afterEvent(int event, int result, Object data) {
+                if (result == SMSSDK.RESULT_COMPLETE) {
+                    // 处理成功的结果
+                    HashMap<String,Object> phoneMap = (HashMap<String, Object>) data;
+                    String country = (String) phoneMap.get("country"); // 国家代码，如“86”
+                    String phone = (String) phoneMap.get("phone"); // 手机号码，如“13800138000”
+                    // TODO 利用国家代码和手机号码进行后续的操作
+                } else {
+                    Toast.makeText(context, "验证失败请重试！", Toast.LENGTH_SHORT).show();
+                    // TODO 处理错误的结果
+                }
+            }
+        });
+        page.show(context);
+    }
+}
